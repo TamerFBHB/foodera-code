59451dee4a1ab84c39f5ab9fce25ae423f9d346e
Tamer Fouad <tamer.fbhb.wd@gmail.com>
2023-07-18 18:57:40 +0300

    first commit

---
+++ src/component/Modals/ModalOrder.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import "./ModalOrder.css";
+import { Form } from "react-bootstrap";
+
+function ModalOrder() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Order Now
+      </Button>
+
+      <Modal className="main" show={show} onHide={handleClose}>
+        <Modal.Header className="header" closeButton>
+          <Modal.Title className="titleModal mb-3 mx-auto">
+            Make Your Order Now!
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="row ">
+            <form>
+              <input className="col-6 me-3" type="text" placeholder="Your name" />
+              <input
+                className="col-5 ms-3 my-3"
+                type="text"
+                placeholder="Your Email "
+              />
+              <input
+                className="col-12 "
+                type="text"
+                placeholder="Your  Adress"
+              />
+              <input
+                className="col-12 my-3"
+                type="number"
+                placeholder="Zep Code"
+              />
+              <input
+                className="col-12"
+                type="number"
+                placeholder="Mobile No."
+              />
+              <Form.Select className="my-3 dropItems">
+                <option>Rainbow Vegetable Sandwich $10.50</option>
+                <option>Vegetarian Burger $9.20</option>
+                <option>Raspberry Stuffed French Toast $12.20</option>
+              </Form.Select>
+            </form>
+          </div>
+        </Modal.Body>
+        <Modal.Footer className="footer">
+
+          <Button  onClick={handleClose}>
+            Send Message
+            <i className="bi bi-send ms-2"></i>
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default ModalOrder;
---
+++ src/component/pages/home/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Header.css";
+import NavHeader from "../navs/NavHeader";
+
+const Header = ({about,explorer,review,home}) => {
+  return (
+    <>
+      <NavHeader aBout={about} explorer={explorer} Review ={review} Home ={home}/>
+      <header>
+        <div className="container " ref={home}>
+          <div className="row  main">
+            <div className="col-md-6  info">
+              <h1 className="title">Good food choices are good investments.</h1>
+              <p className="my-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                et purus a odio finibus bibendum amet leo.
+              </p>
+              <button className="btn">
+                Order Now
+                <i className="bi bi-basket2-fill ms-2"></i>
+              </button>
+              <button className="btnLearn">
+                Learn More
+                <i className="bi bi-caret-right ms-2"></i>
+              </button>
+            </div>
+
+            <div className=" col-md-6 "></div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
